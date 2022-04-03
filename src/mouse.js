efe303a3a8df28e9361259da8063dd7f98290f4d
thomas-auffroy <thomas.auffroy@gmail.com>
2022-04-03 15:51:34 +0200

    adjust cursor position to center of blob

--- src/mouse.js
+++ src/mouse.js
@@ -11,7 +11,7 @@ let speedY = 0;
 
 document.body.addEventListener("mousemove", function(e) {
 
-	cursor.style = `top: ${e.clientY - heightC + blob.getRadius()*scaleFactor - blob.getYPos()}px; left: ${e.clientX-widthC/2}px`;
+	cursor.style = `top: ${e.clientY - heightC + blob.getRadius()*scaleFactor}px; left: ${e.clientX-widthC/2}px`;
 
     if (timestamp === null) {
         timestamp = Date.now();
