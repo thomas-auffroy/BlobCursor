482815bd1e4b0edb5be471fcfd4d22b4f30857b5
thomas-auffroy <thomas.auffroy@gmail.com>
2022-04-04 11:36:16 +0200

    adjust cursor

--- src/mouse.js
+++ src/mouse.js
@@ -11,7 +11,7 @@ let speedY = 0;
 
 document.body.addEventListener("mousemove", function(e) {
 
-	cursor.style = `top: ${e.clientY - heightC + blob.getRadius()*scaleFactor}px; left: ${e.clientX-widthC/2}px`;
+	cursor.style = `top: ${e.clientY - heightC + blob.getRadius()*scaleFactor + 3}px; left: ${e.clientX-widthC/2}px`;
 
     if (timestamp === null) {
         timestamp = Date.now();
@@ -31,4 +31,11 @@ document.body.addEventListener("mousemove", function(e) {
     timestamp = now;
     lastMouseX = e.screenX;
     lastMouseY = e.screenY;
+});
+
+document.body.addEventListener("mousemove", () => {
+    tmpForce = new Vector(0.0, 0.0);
+    tmpForce.setX(speedX * 2 * (Math.random() * 0.75 + 0.25));
+    tmpForce.setY(speedY * 2 * (Math.random() * 0.75 + 0.25));
+    blob.addForce(tmpForce);
 });
